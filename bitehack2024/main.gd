2cfb1f0edbb60ca480d5a5fb1e76619c528d0d65
Pixel48 <piksel48@gmail.com>
2024-12-14 21:52:27 +0100

    Add mouse input cancel

--- bitehack2024/main.gd
+++ bitehack2024/main.gd
@@ -20,14 +20,22 @@ func _process(delta: float) -> void:
 		isInputLockerNext = true
 		firstPosition = position
 		print("Move stack up pressed:", firstPosition)
-	if Input.is_action_just_released("ui_focus_next") && isInputLockerNext:
-		isInputLocked = false
-		print("Move stack up released:", position)
+	if Input.is_action_just_released("ui_focus_next") && isInputLocked:
+		if isInputLockerNext:
+			isInputLocked = false
+			print("Move stack up released:", position)
+		else:
+			isInputLocked = false
+			print("Operation canceled!")
 	if Input.is_action_just_pressed("ui_focus_prev") && !isInputLocked:
 		firstPosition = position
 		isInputLocked = true
 		isInputLockerNext = false
 		print("Move stack down pressed:", firstPosition)
-	if Input.is_action_just_released("ui_focus_prev") && !isInputLockerNext:
-		isInputLocked = false
-		print("Move stack down released:", position)
+	if Input.is_action_just_released("ui_focus_prev") && isInputLocked:
+		if !isInputLockerNext:
+			isInputLocked = false
+			print("Move stack down released:", position)
+		else:
+			isInputLocked = false
+			print("Operation canceled!")
