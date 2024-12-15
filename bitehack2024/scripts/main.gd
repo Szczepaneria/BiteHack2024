f9c91b8e2660e8a49ab92b99f56cfa77cf38b946
Pixel48 <piksel48@gmail.com>
2024-12-15 08:10:02 +0100

    Tweak splash screens

--- bitehack2024/scripts/main.gd
+++ bitehack2024/scripts/main.gd
@@ -1,6 +1,14 @@
 extends Node
+@onready var curtain: ColorRect = $Curtain
+@export var fade_out_time: float = .25
+
+#func curtain_fade_out() -> void:
+	#var tween: Tween = create_tween()
+	#tween.tween_property(curtain, 'modulate:a', .0, fade_out_time)
+	#await tween.finished
 
 func _ready() -> void:
+	#curtain_fade_out()
 	var window = get_window()
 	window.move_to_center()
 	window.grab_focus()
--- bitehack2024/scripts/splash_screen_controller.gd
+++ bitehack2024/scripts/splash_screen_controller.gd
@@ -6,21 +6,23 @@ extends Control
 @export var pause_time: float = 1.5
 @export var fade_out_time: float = 1.5
 @export var out_time: float = .5
-@onready var splash_screen_container: CenterContainer = $"."
 
 var splash_screens: Array
 
+func get_fade_out_time() -> float:
+	return fade_out_time
+
 func get_splash_screens() -> void:
-	splash_screens = splash_screen_container.get_children()
+	splash_screens = self.get_children()
 	for screen in splash_screens: screen.modulate.a = 0.0
 
 func fade() -> void:
 	for screen in splash_screens:
 		var tween = self.create_tween()
 		tween.tween_interval(in_time)
-		tween.tween_property(screen, "modulate:a", 1.0, fade_in_time)
+		tween.tween_property(screen, 'modulate:a', 1.0, fade_in_time)
 		tween.tween_interval(pause_time)
-		tween.tween_property(screen, "modulate:a", 0.0, fade_out_time)
+		tween.tween_property(screen, 'modulate:a', 0.0, fade_out_time)
 		tween.tween_interval(out_time)
 		await tween.finished
 	get_tree().change_scene_to_packed(load_scene)
